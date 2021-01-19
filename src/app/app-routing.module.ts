67649ed543c76910b37b62f73e577e7d24453549
Arun Kumar <k.arun1603.com@gmail.com>
2021-01-19 22:56:51 +0530

    small changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 
 const routes : Routes  = [
+  {path:'',redirectTo:"/Login",pathMatch:"full"},
   {path: 'Dashboard',  loadChildren: './layout/default-layout/default-layout.module#DefaultLayoutModule'},  
   {path: 'Login', component : LoginComponent},             
   {path: 'register', component : RegisterComponent},
